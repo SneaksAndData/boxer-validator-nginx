2fdd665b8718b27d82ee038472819e4e05c90841
Vitalii Savitskii <visa@ecco.com>
2024-08-28 10:00:07 +0200

    Init the boxer-validator-nginx project

--- src/http/urls.rs
+++ src/http/urls.rs
@@ -2,6 +2,6 @@ use actix_web::{get, HttpRequest};
 
 // Dummy implementation of the token endpoint
 #[get("/token/review")]
-async fn token_review(req: HttpRequest) -> String {
+async fn token_review(_: HttpRequest) -> String {
     "dummy endpoint".to_string()
     }
