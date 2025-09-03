d8097d1a99e472d75f5e75a7d78d8282e942a244
Vitalii Savitskii <visa@ecco.com>
2025-09-03 14:52:25 +0200

    Implement the same for validator

--- src/main.rs
+++ src/main.rs
@@ -19,7 +19,11 @@ use anyhow::Result;
 use boxer_core::services::backends::kubernetes::repositories::schema_repository::SchemaRepository;
 use boxer_core::services::backends::BackendConfiguration;
 use boxer_core::services::observability::composed_logger::ComposedLogger;
+use boxer_core::services::observability::open_telemetry;
+use boxer_core::services::observability::open_telemetry::metrics::init_metrics;
+use boxer_core::services::observability::open_telemetry::tracing::init_tracer;
 use boxer_core::services::service_provider::ServiceProvider;
+use env_filter::Builder;
 use log::info;
 use opentelemetry_instrumentation_actix_web::RequestTracing;
 use std::sync::Arc;
@@ -28,17 +32,42 @@ use utoipa_swagger_ui::SwaggerUi;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
-    ComposedLogger::new()
-        // .with_logger(open_telemetry::logging::init_logger()?)
-        .with_logger(Box::new(env_logger::Builder::from_default_env().build()))
-        .with_global_level(log::LevelFilter::Info)
-        .init()?;
+    let mut builder = Builder::new();
+
+    let filter = if let Ok(ref filter) = std::env::var("RUST_LOG") {
+        builder.parse(filter);
+        builder.build()
+    } else {
+        Builder::default().parse("info").build()
+    };
 
     let cm = AppSettings::new()?;
+
+    let logger = ComposedLogger::new();
+    let logger = {
+        if cm.opentelemetry.log_settings.enabled {
+            logger.with_logger(open_telemetry::logging::init_logger()?)
+        } else {
+            logger
+        }
+    };
+
+    logger
+        .with_logger(Box::new(env_logger::Builder::from_default_env().build()))
+        .with_global_level(filter)
+        .init()?;
+
     info!("Configuration manager started");
 
-    // open_telemetry::tracing::init_tracer()?;
+    if cm.opentelemetry.tracing_settings.enabled {
+        info!("Tracing is enabled, starting tracer...");
+        init_tracer()?;
+    }
 
+    if cm.opentelemetry.metrics_settings.enabled {
+        info!("Metrics is enabled, starting metrics...");
+        init_metrics()?;
+    }
     let current_backend = backends::new()
         .configure(&cm.backend.kubernetes, cm.instance_name.clone())
         .await?;
